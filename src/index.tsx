cff9a87882d22a784053a8ad9e8603bf62f39190
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2020-10-10 19:18:00 +0800

    add css & style loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
 import Hello from "./components/Hello";
+import './index.css';
 
 ReactDOM.render(
   <Hello />,
+  // <div className="test">hello!!!!!</div>,
   document.getElementById('root') as HTMLElement
 );
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "ts-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
     ]
   },
 
