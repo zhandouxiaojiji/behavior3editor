b4021e487c595e1e6c8f95588aa361d73552428d
codetypes <codetypes@gmail.com>
2024-07-16 19:53:12 +0800

    update batch example

--- sample/scripts/test.js
+++ sample/scripts/test.js
@@ -2,15 +2,45 @@
 /* 
     如果有processTree，则先运行prcessTree，如果有返回，则进行后面的针对每个节点运行processNode
     如果没有定义processTree，则直接针对每个节点运行processNode
+
+    export interface TreeModel {
+        name: string;
+        desc?: string;
+        export?: boolean;
+        root: NodeModel;
+    }
+
+    export interface NodeModel {
+        id: number;
+        name: string;
+        desc?: string;
+        args?: { [key: string]: any };
+        input?: string[];
+        output?: string[];
+        children?: NodeModel[];
+        debug?: boolean;
+        disabled?: boolean;
+        path?: string;
+    }
+
+    interface BatchScript {
+        processTree?(tree: TreeModel, path: string): TreeModel;
+
+        processNode?(node: NodeModel, tree: TreeModel): NodeModel;
+    }
 */
 ({
-    processTree: (tree) => {
-        console.log(`processTree ${tree.name}`);
-        if (tree.name == "hero") {
-            return tree;
-        }
-    },
-    processNode: (node, tree) => {
-        console.log(`processNode ${tree.name}.${node.id}`);
+  processTree: (tree) => {
+    console.log(`processTree ${tree.name}`);
+    if (tree.name == "hero") {
+      return tree;
     }
-})
+  },
+  processNode: (node, tree) => {
+    console.log(`processNode ${tree.name}.${node.id}`);
+    if (node.name == "GetPos") {
+      return null;
+    }
+    return node;
+  },
+});
